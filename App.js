e5274888b179d9a2a513d950c7a270c27988dc2b
Apoorva Rao <100127532+Rao-Apoorva@users.noreply.github.com>
2022-12-25 14:55:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Info from "./Component/Info"
+import About from "./Component/About"
+import Interest from "./Component/Interest"
+import Footer from "./Component/Footer"
+
+export default function App()
+{
+    return(
+        <>
+            <Info />
+            <About />
+            <Interest />
+            <Footer />
+        </>
+    )
+}
