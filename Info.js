3845b628800f13799f792eb83d7470f3acb83239
Apoorva Rao <100127532+Rao-Apoorva@users.noreply.github.com>
2022-12-25 14:56:07 +0530

    Add files via upload

---
+++ Info.js
@@ -0,0 +1,16 @@
+import React from "react"
+export default function Info()
+{
+    return (
+<div>
+    <img src="https://img.freepik.com/premium-photo/woman-portrait-park-anime-manga-style_691560-1170.jpg?w=2000" alt="image" width="250px" className="img"/>
+    <header className="header">
+    <h1 className="name">Apoorva Rao</h1>
+    <h3  className="title">Frontend Devopler</h3>
+    <h4  className="website">www.raoapoorva.com</h4>
+    <button type="button"  className="email">Eamil</button>
+    <button type="button"  className="linkedin">LinkedIn</button>
+    </header>
+</div>
+    )
+}
---
+++ Interest.js
@@ -0,0 +1,12 @@
+import React from "react"
+export default function Interest()
+{
+    return(
+ <>
+    <h2 className="interest">Interest</h2>
+    <p className="interestpara">
+     Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+    </p>
+</>
+    )
+}
